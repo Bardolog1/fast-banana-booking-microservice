32fbc4307d617929823174bf1e5c355707b1051e
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-05-25 21:22:38 -0500

    init project

---
+++ src/main/java/com/FastBanana/bookingmicroservice/BookingMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.FastBanana.bookingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BookingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMicroserviceApplication.class, args);
+	}
+
+}
